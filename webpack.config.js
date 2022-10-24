96397080d17fc4560002c3578c3a454293101f42
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-10-24 14:02:41 +0100

    Use ESLinting

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   mode: 'development',
@@ -6,6 +6,6 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+    path: path.resolve(__dirname, 'dist')
+  }
+}
