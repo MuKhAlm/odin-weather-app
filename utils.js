7491dcb6db7c82ff857dd5a79b5e88757893f110
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-10-24 21:15:10 +0100

    Move API functions from index.js to utils.js
    Since index.js should be only concerned with displaying the main page, it was judged that moving functions that deal with API's to its own module is better.

---
+++ utils.js
@@ -0,0 +1,60 @@
+const openWeatherKey = 'cb500be0c41e81f297b89263ec67329a'
+
+/**
+ * Hits 'Open Weather' APIs to get weather data for a given location,
+ *
+ * @param {String} location  a name of a city, zipcode or country,
+ *
+ * @return {Object} weather data for the given location.
+ */
+async function getWeatherData (location) {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${openWeatherKey}`, {
+      mode: 'cors'
+    }
+  )
+  const data = await response.json()
+  return data
+}
+
+/**
+ * Process given weather data and returns an object containing the data required by the app,
+ *
+ * The data to be returned contains the weather condition, temp (temprature), feelsLike(the Temp perceived by humans) and humidity (in percent),
+ *
+ * @param {Object} data  weather data,
+ *
+ * @return {Object} processed weather data.
+ */
+function getProcessedWeatherData (data) {
+  // Temperature properties are converted from Kelvin to Celcius
+  const processedData = {
+    condition: data.weather[0].main,
+    temp: data.main.temp - 273.15,
+    feelsLike: data.main.feels_like - 273.15,
+    humidity: data.main.humidity
+  }
+  return processedData
+}
+
+/**
+ * Console logs weather data object,
+ *
+ * @param {string} location   name of the desired location.
+ */
+async function clgWeatherData (location) {
+  const data = await getWeatherData(location)
+  console.log(data)
+}
+clgWeatherData('London')
+
+/**
+ * Console logs weather data object,
+ *
+ * @param {string} location   name of the desired location.
+ */
+async function clgProcessedWeatherData (location) {
+  const data = await getWeatherData(location)
+  console.log(getProcessedWeatherData(data))
+}
+clgProcessedWeatherData('London')
