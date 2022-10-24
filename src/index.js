2678249c4e7965b060855bd993f3b4fb4c4f4ce7
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-10-24 14:45:23 +0100

    Add a function for requesting weather data
    getWeatherData is responsible for getting weather data from the OpenWeather APIs

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const openWeatherKey = 'cb500be0c41e81f297b89263ec67329a'
+
+/**
+ * Hits Open Weather APIs to get weather data for a given location,
+ *
+ * @param {String} location  a name of a city, zipcode or country,
+ *
+ * @return {Object} weather data for the given location.
+ */
+async function getWeatherData (location) {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${openWeatherKey}`, {
+      mode: 'cors'
+    }
+  )
+  const data = await response.json()
+  return data
+}
