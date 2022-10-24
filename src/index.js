f4ce360b5c46d7d6ffa82fcaee080a0260f6e2a6
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-10-24 17:42:20 +0100

    Add getProcessedData function
    The function is responsible for processing data fetched from OW APIs and returning only the bits required by the application.
    The weather data required was decided to be Temp, feelsLike(the Temp perceived by humans), humidity (in percent) and the weather condition.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const openWeatherKey = 'cb500be0c41e81f297b89263ec67329a'
 
 /**
- * Hits Open Weather APIs to get weather data for a given location,
+ * Hits 'Open Weather' APIs to get weather data for a given location,
  *
- * @param {string} location  a name of a city, zipcode or country,
+ * @param {String} location  a name of a city, zipcode or country,
  *
  * @return {Object} weather data for the given location.
  */
@@ -18,6 +18,24 @@ async function getWeatherData (location) {
 }
 
 /**
+ * Process given weather data and returns an object containing the data required by the app,
+ *
+ * @param {Object} data  weather data,
+ *
+ * @return {Object} processed weather data.
+ */
+function processWeatherData (data) {
+  // Temperature properties are converted from Kelvin to Celcius
+  const processedData = {
+    condition: data.weather.main,
+    temp: data.main.temp - 273.15,
+    feelsLike: data.main.feels_like - 273.15,
+    humidity: data.main.humidity
+  }
+  return processedData
+}
+
+/**
  * Console logs weather data object,
  *
  * @param {string} location   name of the desired location.
@@ -26,4 +44,4 @@ async function clgWeatherData (location) {
   const data = await getWeatherData(location)
   console.log(data)
 }
-clgWeatherData('London')
+clgWeatherData('baghdad')
